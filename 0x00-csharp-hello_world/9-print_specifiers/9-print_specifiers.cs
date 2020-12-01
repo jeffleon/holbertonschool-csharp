9fa0ee20000e0c45a572a858819c3e578d719f80
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-11-30 23:37:11 -0500

    [UPDATE] task 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -5,6 +5,7 @@ class Program
 {
 	static void Main(string[] args)
 	{
+        
 		double percent = .7553;
 		double currency = 98765.4321;
         Console.WriteLine("Percent: {0:0.00%}", percent);
