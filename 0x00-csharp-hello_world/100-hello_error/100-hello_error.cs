f2231024faf93472005baa4cfc90959d9280ced4
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-11-30 23:50:29 -0500

    [ADD] advanced task

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,16 @@
+﻿  
+using System;
+using System.Numerics;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exitErrorCode = 1;
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Environment.ExitCode = exitErrorCode;
+        }
+    }
+}
