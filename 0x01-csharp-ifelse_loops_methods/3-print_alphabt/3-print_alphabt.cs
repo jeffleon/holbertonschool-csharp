17cfc71bf5e2cde923ce9e0742aaecd3f9c62240
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-12-01 22:49:30 -0500

    [ADD] task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet == 'q' || alphabet == 'e')
+                {
+                    continue;
+                }
+                Console.Write(alphabet);
+            }
+        }
+    }
+}
