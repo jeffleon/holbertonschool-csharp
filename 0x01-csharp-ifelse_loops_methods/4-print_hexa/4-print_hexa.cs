9b893fe9640b86b026d200f33cd0870de1cae288
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-12-01 22:55:32 -0500

    [ADD] task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int alphabet = 0; alphabet <= 98; alphabet++)
+            {
+                Console.WriteLine("{0} = 0x{1}" , alphabet, alphabet.ToString("x"));
+            }
+        }
+    }
+}
