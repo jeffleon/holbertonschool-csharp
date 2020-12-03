f9575d00daecf1a5ee428518bbec0d1aaf025e93
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-12-02 23:22:38 -0500

    [ADD] task 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index >= array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        return array[index];
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {10, 17, -8, 4, -12, 7, 0, 1, -1, -9};
+
+        Console.WriteLine("Element at index {0} is {1}", 4, Array.elementAt(array, 4));
+        Console.WriteLine("Element at index {0} is {1}", 9, Array.elementAt(array, 9));
+        Console.WriteLine("Element at index {0} is {1}", 0, Array.elementAt(array, 0));
+        Array.elementAt(array, -7);
+    }
+}
